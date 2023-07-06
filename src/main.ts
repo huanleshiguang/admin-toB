035c360da399d98b35d79836b8207f448931d462
QMZhao <zhao971462054@163.com>
2023-07-06 15:11:48 +0800

    fix: 删除全局引用antd样式

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@
  * @Autor: QMZhao
  * @Date: 2021-09-08 23:54:23
  * @LastEditors: QMZhao zhao971462054@163.com
- * @LastEditTime: 2023-07-05 18:34:08
+ * @LastEditTime: 2023-07-06 10:20:41
  * @Description:
  * @FilePath: /vue-vite-template/src/main.ts
  */
 
 import App from "./App.vue";
 
-import "ant-design-vue/dist/antd.css";
+// import "ant-design-vue/dist/antd.css";
 
 import router from "/@/router";
 
