3ed76d4893837f47a08d50c09b9204f994c2d2bd
ZhouHao <joehall@foxmail.com>
2023-07-11 17:28:24 +0800

    feat: 参数/床位管理

--- src/store/common/routerList.ts
+++ src/store/common/routerList.ts
@@ -2,7 +2,7 @@
  * @Author: QMZhao
  * @Description:
  * @Date: 2023-02-11 15:51:21
- * @LastEditTime: 2023-02-17 15:23:33
+ * @LastEditTime: 2023-07-11 14:33:25
  * @Reference:
  */
 import { ImenuList } from "/@/model/common";
@@ -36,6 +36,20 @@ export const useNavMenuList = defineStore("navMenuList", () => {
           icon: "role",
           permission: "system_role",
           path: "/system/role"
+        },
+        {
+          id: 1005,
+          label: "运行参数",
+          icon: "param",
+          permission: "system_role",
+          path: "/system/param"
+        },
+        {
+          id: 1006,
+          label: "床位管理",
+          icon: "role",
+          permission: "system_role",
+          path: "/system/bunk"
         }
       ]
     }
