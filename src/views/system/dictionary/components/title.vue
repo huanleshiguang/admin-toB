910b4c09c86a8f362d75dd5571d841b6e75b3e0c
dengsy <1187164723@qq.com>
2023-07-12 14:02:14 +0800

    commit test

---
+++ src/views/system/dictionary/components/title.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="title">{{ title }}</p>
+</template>
+
+<script setup lang="ts" name="TableTitle">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 16px;
+  font-weight: 500;
+}
+</style>
