d11575148ce00e60186ced1662a0ad2b93f777fc
QMZhao <zhao971462054@163.com>
2023-07-12 16:51:07 +0800

    feat: 自动导入组件更新

--- components.d.ts
+++ components.d.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: ZhouHao joehall@foxmail.com
- * @Date: 2023-07-11 17:29:31
- * @LastEditors: ZhouHao joehall@foxmail.com
- * @LastEditTime: 2023-07-11 17:59:17
- * @FilePath: \servious-illness-admin\components.d.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /* eslint-disable */
 /* prettier-ignore */
 // @ts-nocheck
@@ -29,19 +21,19 @@ declare module 'vue' {
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
-    ElRow: typeof import('element-plus/es')['ElRow']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
-    ElTable: typeof import('element-plus/es')['ElTable']
-    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     FlexSpacer: typeof import('./src/components/common/flex-spacer.vue')['default']
     IEpLocation: typeof import('~icons/ep/location')['default']
     LoginForm: typeof import('./src/components/login/LoginForm.vue')['default']
