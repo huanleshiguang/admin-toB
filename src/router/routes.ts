2859fc02deee18a59bffc8e1a31f9a40d1859146
ZhouHao <joehall@foxmail.com>
2023-07-12 19:32:12 +0800

    fix: route_path更改

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: ZhouHao joehall@foxmail.com
  * @Date: 2023-07-11 11:18:06
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-07-12 10:30:36
+ * @LastEditors: ZhouHao joehall@foxmail.com
+ * @LastEditTime: 2023-07-12 19:26:21
  * @FilePath: \servious-illness-admin\src\router\routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,7 +28,7 @@ export const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: 'param',
-        component: () => import('/@/views/system/param.vue'),
+        component: () => import('/@/views/system/param/index.vue'),
         name: 'Param',
         meta: { title: '运行参数', icon: 'dashboard', affix: true, requireAuth: true }
       },
@@ -40,7 +40,7 @@ export const routes: Array<RouteRecordRaw> = [
       },
       {
         path: 'bunk',
-        component: () => import('/@/views/system/bunk.vue'),
+        component: () => import('/@/views/system/bunk/index.vue'),
         name: 'Bunk',
         meta: { title: '床位管理', icon: 'dashboard', affix: true, requireAuth: true }
       },
