4333d1cf7eb63df2cb57f982c79fc0b233ca566a
ZhouHao <joehall@foxmail.com>
2023-07-13 15:26:30 +0800

    fit:table布局修复

--- src/api/system/user.ts
+++
@@ -1,3 +0,0 @@
-import request from '/@/utils/request'
-//获取院区信息
-export const apiGethosptAreaInfo = () => request({url:'/Organization/OrgManage/GetOrgAreaInfos'})
--- src/views/system/bunk/components/BedEquipment.vue
+++ src/views/system/bunk/components/BedEquipment.vue
@@ -2,7 +2,7 @@
  * @Author: ZhouHao joehall@foxmail.com
  * @Date: 2023-07-12 14:40:09
  * @LastEditors: ZhouHao joehall@foxmail.com
- * @LastEditTime: 2023-07-13 14:27:39
+ * @LastEditTime: 2023-07-13 15:11:07
  * @FilePath: \servious-illness-admin\src\views\system\bunk\components\BedEquipment.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -198,9 +198,8 @@ const handleClose = (key: string, keyPath: string[]) => {
 .container {
   height: calc(100% - 36px);
 }
-
 .main-container {
-  height: 600px;
+  height: 550px;
 }
 
 .box-card {
