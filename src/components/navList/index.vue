44905b7cb657cbc7e1042662c846408a0ae2ebf9
quanmingmin <401748839@qq.com>
2023-07-17 18:16:51 +0800

    fix layout

--- src/components/navList/index.vue
+++ src/components/navList/index.vue
@@ -2,14 +2,14 @@
 	<div class="navBox">
 		<el-scrollbar height="42px" :always="true">
 			<el-button-group class="flex flex-ac flex-nowrap">
-				<el-button :type="data.current === index ? 'primary' : 'default'" v-for="(item,index) in navList"
+				<el-button :type="data.current === index ? 'primary' : 'default'" v-for="(item,index) in data.navList"
 					:key="index" @click="changeNav(index)">{{item.name}}</el-button>
 			</el-button-group>
 		</el-scrollbar>
 	</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 	const props = defineProps({
 		navList: {
 			type: Array,
@@ -44,10 +44,13 @@
 	})
 
 	const data = reactive({
-		current: 0
+		current: 0,
+		navList:[] as any
 	})
+	
+	data.navList = props.navList;
 
-	const changeNav = (index) => {
+	const changeNav = (index:number) => {
 		data.current = index
 	}
 </script>
