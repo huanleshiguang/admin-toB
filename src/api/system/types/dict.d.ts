161c69df56e5f186e66aa98f5d3037fd3c00e8e4
dengsy <1187164723@qq.com>
2023-07-19 17:22:56 +0800

    feat: 增加基础字典和值域的接口联调

---
+++ src/api/system/types/dict.d.ts
@@ -0,0 +1,16 @@
+export interface DictInfo {
+  id?: string;
+  dictCode: string;
+  dictName: string;
+  dictEnName: string;
+  dictEnAbbr: string;
+  remark: string;
+}
+export interface DictValueInfo {
+  id?: string;
+  dictId: string;
+  rangeName: string;
+  rangeEnName: string;
+  rangeValue: string;
+  remark: string;
+}
