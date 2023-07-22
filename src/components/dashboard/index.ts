bf83231a58d79e08035aa40481c53ec9d62e4e45
QMZhao <zhao971462054@163.com>
2023-07-22 18:11:18 +0800

    style: 删除多余代码

--- src/components/dashboard/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: QMZhao
- * @Description: 
- * @Date: 2022-10-31 15:04:40
- * @LastEditTime: 2022-10-31 15:04:54
- * @Reference: 
- */
-export {default as BarChart} from "./BarChart.vue"
