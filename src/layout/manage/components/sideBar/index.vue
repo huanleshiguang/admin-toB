3672f98920c95fbc546dd02e69489faff5f6507f
QMZhao <zhao971462054@163.com>
2023-08-15 10:38:09 +0800

    feat: 系统菜单折叠栏样式优化

--- src/layout/manage/components/sideBar/index.vue
+++ src/layout/manage/components/sideBar/index.vue
@@ -2,12 +2,11 @@
  * @Author: QMZhao
  * @Description: 
  * @Date: 2022-08-17 17:30:49
- * @LastEditTime: 2023-08-11 16:03:06
+ * @LastEditTime: 2023-08-14 18:43:28
  * @Reference: 
 -->
 <script lang="ts" setup>
 import SideBarItem from './sideBarItem.vue';
-import SideBarToggle from '/@/layout/manage/components/Navbar/SideBarToggle.vue';
 import { useMenuCollapse } from '/@/store/common/useCommon';
 
 const store = useMenuCollapse();
@@ -17,14 +16,6 @@ const getCollapaseStatus = computed(() => store.isCollapse);
 
 <template>
   <div class="h_100 flex flex-col sidebar-content">
-    <div
-      :class="[
-        'company-cell h-32 uno-uno-flex-y-center cursor-pointer',
-        getCollapaseStatus ? 'sidebar-header_collapse' : 'sidebar-header_expand'
-      ]"
-    >
-      <SideBarToggle />
-    </div>
     <!-- 侧边栏 -->
     <div class="side-bar-list">
       <el-scrollbar>
@@ -37,55 +28,7 @@ const getCollapaseStatus = computed(() => store.isCollapse);
 <style scoped lang="scss">
 .sidebar-content {
   box-sizing: border-box;
-  .sidebar-header_expand {
-    width: 151px;
-    background-color: $color-border;
-    @include cursor;
-  }
-  .sidebar-header_collapse {
-    width: 40px;
-  }
-  .company-cell {
-    transition: all 0.3s ease-in;
-    .logo {
-      width: 50px;
-      height: 50px;
-      .logo-image {
-        border-radius: 50%;
-      }
-    }
-    .company-name-position {
-      position: absolute;
-      top: 40px;
-    }
-  }
-  .company-name-box {
-    min-width: 100px;
-    max-width: 110px;
-    text-align: center;
-    font-size: $font-size-14;
-    @include text-ellipsis;
-    transition: all 0.1s 0.3s cubic-bezier(0.18, -0.1, 0.04, 1.15);
 
-    .company-name {
-      display: inline-block;
-      transition: all 0.1s 0.3s cubic-bezier(0.18, -0.1, 0.04, 1.15);
-    }
-    .hide-name {
-      opacity: 0;
-    }
-    .show-name {
-      opacity: 1;
-    }
-  }
-  .translate-hide {
-    left: 0px;
-    transform: translate(100%, -50%);
-  }
-  .translate-show {
-    left: 70px;
-    transform: translate(-10%, -50%);
-  }
   .side-bar-list {
     flex: 1;
     height: 100%;
@@ -95,22 +38,6 @@ const getCollapaseStatus = computed(() => store.isCollapse);
     :deep(.el-menu--collapse) {
       width: 40px;
     }
-  }
-  .menu-btn {
-    height: calc(100% - 730px);
-    .menu-btn-icon {
-      width: 40px;
-      height: 40px;
-      font-size: 20px;
-      cursor: pointer;
-      transition: all 0.2s ease;
-    }
-  }
-  .expand-icon {
-    width: 70px;
-  }
-  .collapse-icon {
-    width: 200px;
   }
 }
 </style>
